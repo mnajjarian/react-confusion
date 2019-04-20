74c54e7834d25b485cba09915982ac04cd92c37c
Mahdi Najjarian <mnajarian@gmail.com>
2019-04-20 12:08:32 +0300

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent'
 import Home from './HomeComponent'
 import Dishdetail from './DishdetailComponent'
 import About from './AboutComponent'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import { Switch, Route, Redirect , withRouter} from 'react-router-dom'
 import { connect } from 'react-redux'
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators'
@@ -71,14 +72,18 @@ class Main extends React.Component {
         return(
             <div >
                 <Header />
-                <Switch>
-                    <Route path="/home" component={Homepage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes.dishes} /> } />
-                    <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
-                    <Redirect to="/home" />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames='page' timeout={300}>
+                        <Switch location={this.props.location} >
+                            <Route path="/home" component={Homepage} />
+                            <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes.dishes} /> } />
+                            <Route path="/menu/:dishId" component={DishWithId} />
+                            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                            <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
+                            <Redirect to="/home" />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         )
